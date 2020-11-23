9c22357f4f09eb74c3a60e13492087be161a1f86
Yihong <JIN011112@outlook.com>
2020-11-23 15:53:52 +0800

    count.c

--- Chapter 13/count.c
+++ Chapter 13/count.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int ch;
+    FILE *fp;
+    unsigned long count = 0;
+    if (argc != 2)
+    {
+        printf("Usage: %s filename\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    if ((fp = fopen(argv[1], "r")) == NULL)
+    {
+        printf("Can't open %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    while ((ch = getc(fp)) != EOF)
+    {
+        putc(ch, stdout);
+        count++;
+    }
+    //fclose(fp);
+    if (fclose(fp) != 0)
+    {
+        printf("Error in closing file %S\n");
+    }
+    printf("FIle %s has %lu characters\n", argv[1], count);
+
+    return 0;
+}
