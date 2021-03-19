6446da5d39ae64c5409cbd1413bb33635bba108e
YIHONG-JIN <JIN011112@outlook.com>
2021-03-19 20:16:20 +0800

    lala

--- Chapter 1/1.1.c
+++ Chapter 1/1.1.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-int main(void)
-{
-    int dogs;
+int main(void) {
+  int dogs;
 
-    printf("How many dogs do you have?\n");
-    scanf("%d", &dogs);
-    printf("So you have %d dog(s)!\n", dogs);
+  printf("How many dogs do you have?\n");
+  scanf("%d", &dogs);
+  printf("So you have %d dog(s)!\n", dogs);
 
-
-    return 0;
+  return 1;
 }
